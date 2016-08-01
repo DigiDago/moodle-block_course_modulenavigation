400f22c91ac048af1999279ef244d5e31d399e8c
clementpr <c.prudhomme@digidago.com>
2016-08-01 13:19:03 +0200

    Update version.php

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'block_course_modulenavigation';
-$plugin->release    = '3.2';
+$plugin->release    = '3.3';
 $plugin->version    = 2016061000;
 $plugin->requires   = 2015102300; // Moodle 3.0 and above
 $plugin->maturity   = MATURITY_STABLE;
