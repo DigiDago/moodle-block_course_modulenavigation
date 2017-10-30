5b540049a39c6bfb3c483bf5decb6055b075dc4d
CrymSonir <syrevenu@sfr.fr>
2017-10-30 17:00:13 +0100

    Remove one author line in GruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,6 @@
  *
  * @package    block_course_modulenavigation
  * @copyright  2016 Digidago <contact@digidago.com><www.digidago.com>
- * @author     Bas Brands | DigiDago
  * @author     Sylvain Renvenu | Nick Papoutsis | Bas Brands | DigiDago
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
