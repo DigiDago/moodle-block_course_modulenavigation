2654ad7af12645af337f39dcd35e57553b6b1e09
CrymSonir <revenu.sylvain@gmail.com>
2020-06-16 15:03:57 +0200

    Remove FR language file since it is handle by Moodle AMOS

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_course_modulenavigation';
 $plugin->release   = '4.1';
-$plugin->version   = 2019052015;
+$plugin->version   = 2020061615;
 $plugin->requires  = 2018051700; // Moodle 3.5 and above.
 $plugin->maturity  = MATURITY_STABLE;
